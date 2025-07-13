7bdc3ebdcd680b5ab61267579cefc33263bea552
Nikos <nikosoikonomou88@gmail.com>
2025-07-14 01:48:20 +0300

    add user routes

---
+++ src/routes/userRoutes.js
@@ -0,0 +1,20 @@
+const express = require('express');
+
+const router = express.Router();
+
+// Only admin can access these routes
+router.get("/admin", (req, res) => {
+    res.json({ message: "Admin access granted" });
+});
+
+// Only admin and manger can access these routes
+router.get("/manager", (req, res) => {
+  res.json({ message: "Manager access granted" });
+});
+
+// All users can access these routes
+router.get("/user", (req, res) => {
+  res.json({ message: "User access granted" });
+});
+
+module.exports = router;
