0d6e9c2d93ef56a44a5803ad90cf7419969ebd77
Nikos <nikosoikonomou88@gmail.com>
2025-07-13 19:18:08 +0300

    define User schema, register contoller basic implementation

--- src/controllers/authController.js
+++ src/controllers/authController.js
@@ -1,12 +1,26 @@
+const bcrypt = require('bcrypt');
+const jwt = require('jsonwebtoken');
 
+const User = require('../models/User');
 
 
 exports.register = async (req, res) => {
     const {username, password, role} = req.body;
-    res.send('User registered');
+    const hashedPassword = await bcrypt.hash(password, 11);
+    const newUser = new User({
+        username,
+        password: hashedPassword,
+        role
+    });
+
+    await newUser.save();
+    
+    res.status(201).json( {
+        message: `User ${username} registered successfully`,
+    });
 };
 
 exports.login = async (req, res) => {
-    // Login logic here
+    const {username, password} = req.body;
     res.send('User logged in');
 };
